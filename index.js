5d14ba95f8382e113bdd41381087845a1fb95a8c
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-22 10:19:48 -0700

    finish map function

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function map(arr, callback) {
-
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    const newItem = callback(item);
+    newArr = [...newArr, newItem];
+  }
+  return newArr;
 }
 
-
 const myArray = [1, 6, 5];
-const newArray = myArray.map((num) => num**2);
+const newArray = map(myArray, (num) => num ** 2);
 console.log(newArray);
